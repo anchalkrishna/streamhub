ae95e741c319f3797c1927637e2cddcb4e6b880d
shamir <shamirshahnoor@gmail.com>
2026-05-17 21:15:02 +0530

    updated settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ad*=!2rna@l(uezj8&i85=j450vetfy=vk%*ue5*i@^@&byv=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
